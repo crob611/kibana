b1cfec386c7714e7f58cb3664788775cd4408612
Corey Robertson <corey.robertson@elastic.co>
2019-06-04 15:12:57 -0400

    test

---
+++ x-pack/plugins/canvas/file.test.ts
@@ -0,0 +1,8 @@
+import { image } from './canvas_plugin_src/functions/common/image';
+
+describe('things', () => {
+  it('should work', () => {
+    console.log(image);
+    expect(true).toBe(true);
+  });
+});
