e1dde64c05f391cca6c55ab60d9dd49fa611eb21
Corey Robertson <corey.robertson@elastic.co>
2020-08-18 09:22:44 -0400

    wip

---
+++ x-pack/plugins/canvas/public/components/add_package_modal/__stories__/packages.stories.tsx
@@ -0,0 +1,45 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { EuiModal } from '@elastic/eui';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import React from 'react';
+import { PackagesGridComponent as Packages } from '../';
+import { elasticLogo } from '../../../../canvas_plugin_src/lib/elastic_logo';
+import { elasticOutline } from '../../../../canvas_plugin_src/lib/elastic_outline';
+
+const packages = [
+  {
+    package: {
+      description: 'This is the description of the Elastic Package',
+      name: 'Elastic Package',
+      title: 'Elastic Package Title',
+      release: 'beta',
+    },
+    packageIcon: elasticLogo,
+  },
+  {
+    package: {
+      description: 'This is the description of the Elastic Outline Package',
+      name: 'Outline Package',
+      title: 'Outline Package Title',
+      release: 'experimental',
+    },
+    packageIcon: elasticOutline,
+  },
+];
+
+const onSelect = action('onSelect');
+/*
+storiesOf('components/Packages', module)
+  .add('default', () => <Packages packages={packages} onSelect={onSelect} />)
+  .add('in a modal', () => (
+    <EuiModal onClose={action('onClose')}>
+      <Packages packages={packages} onSelect={onSelect} />
+    </EuiModal>
+  ));
+*/
